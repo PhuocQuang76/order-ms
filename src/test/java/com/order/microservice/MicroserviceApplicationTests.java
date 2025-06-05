c67c09e9bcfcb39260d29b26a96de086937ac263
PhuocQuang76 <quanghongphuoc@yahoo.com>
2025-06-05 00:18:00 -0700

    fix test class to clean install

--- src/test/java/com/order/microservice/MicroserviceApplicationTests.java
+++ src/test/java/com/order/microservice/MicroserviceApplicationTests.java
@@ -2,12 +2,11 @@ package com.order.microservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.order.ms.OrderMsApplication;  // Import the main application class
 
-@SpringBootTest
+@SpringBootTest(classes = OrderMsApplication.class)
 class MicroserviceApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
