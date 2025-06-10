836f3ebe080adc5cdce54610e5fa649227dd8aa9
PhuocQuang76 <quanghongphuoc@yahoo.com>
2025-06-10 16:12:59 -0700

    add Deployment.yaml

--- src/main/java/com/order/ms/controller/OrderController.java
+++ src/main/java/com/order/ms/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.order.ms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
